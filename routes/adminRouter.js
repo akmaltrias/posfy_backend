158945939738f59c85673f64dc99248909cc51e1
Faiq Hasan <faiqvarian@gmail.com>
2022-03-15 10:13:23 +0700

    buat admin create

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,8 +1,11 @@
 import express from "express";
+import multer from "multer";
 import controller from "../controller/index.js";
 
 const route = express.Router();
+const upload = multer()
 
 route.get("/admin", controller.admin.getAllAdmin);
+route.post("/admin", upload.none() , controller.admin.createAdmin);
 
 export default route;
