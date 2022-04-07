8f785e07e34587510d414247a5dab527b2df5c60
Sahrul Ramdani <ramdanis937@gmail.com>
2022-04-07 15:01:18 +0700

    dikit lagi

--- controller/transaksiCont.js
+++ controller/transaksiCont.js
@@ -1,96 +1,97 @@
-import sequelize from "sequelize";
-import db from "../config/database.js";
-import model from "../model/index.js";
-import moment from "moment";
+import sequelize from 'sequelize';
+import db from '../config/database.js';
+import model from '../model/index.js';
+import moment from 'moment';
 
 const controller = {};
 
-controller.getTransaksi = async function(req, res){
-    try {
-        const [transaksi] = await db.query("SELECT * FROM riwayat_transaksi WHERE tgl_trans = (:tgl_transaksi)", {
-            replacements: {tgl_transaksi: req.body.tgl_transaksi}
-        });
+controller.getTransaksi = async function (req, res) {
+  try {
+    const [transaksi] = await db.query('SELECT * FROM riwayat_transaksi WHERE tgl_trans = (:tgl_transaksi)', {
+      replacements: { tgl_transaksi: req.body.tgl_transaksi },
+    });
 
-        if(transaksi.length > 0 ){
-            res.status(200).json({
-                message: "Data List Transaksi Ditemukan",
-                data : transaksi
-            })
-        }else{
-            res.status(200).json({
-                message: "Data Tidak Ada",
-                data : []
-            })
-        }
-    } catch (error) {
-        res.status(404).json({
-            message: error.message
-        })
+    if (transaksi.length > 0) {
+      res.status(200).json({
+        message: 'Data List Transaksi Ditemukan',
+        data: transaksi,
+      });
+    } else {
+      res.status(200).json({
+        message: 'Data Tidak Ada',
+        data: [],
+      });
     }
-}
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
-controller.getDetailTransaksi = async function(req, res){
-    try {
-        const [detTransaksi] = await db.query("SELECT * FROM view_detail_transaksi WHERE id_transaksi=(:id)", {
-            replacements: {id: req.body.id_transaksi}
-        });
+controller.getDetailTransaksi = async function (req, res) {
+  try {
+    const [detTransaksi] = await db.query('SELECT * FROM view_detail_transaksi WHERE id_transaksi=(:id)', {
+      replacements: { id: req.body.id_transaksi },
+    });
 
-        if(detTransaksi.length > 0 ){
-            res.status(200).json({
-                message: "Data List Detail Transaksi Ditemukan",
-                data : detTransaksi
-            })
-        }else{
-            res.status(200).json({
-                message: "Data Tidak Ada",
-                data : []
-            })
-        }
-    } catch (error) {
-        res.status(404).json({
-            message: error.message
-        })
+    if (detTransaksi.length > 0) {
+      res.status(200).json({
+        message: 'Data List Detail Transaksi Ditemukan',
+        data: detTransaksi,
+      });
+    } else {
+      res.status(200).json({
+        message: 'Data Tidak Ada',
+        data: [],
+      });
     }
-}
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
-controller.createTransaksi = async function(req, res){
-    try{
-        const [result] = await db.query("SELECT id_transaksi() as id");
+controller.createTransaksi = async function (req, res) {
+  try {
+    const [result] = await db.query('SELECT id_transaksi() as id');
 
     //    console.log(result[0].id)
 
-        const data = {
-            id_transaksi: result[0].id,
-            id_user: req.body.id_user,
-            tgl_transaksi: moment().format("YYYY-MM-DD HH:mm:ss")
-        }
-        
-        console.log(data);
+    const data = {
+      id_transaksi: result[0].id,
+      id_user: req.body.id_user,
+      tgl_transaksi: moment().format('YYYY-MM-DD HH:mm:ss'),
+    };
 
-        let transaksi = await model.transaksi.create(data);
+    console.log(data);
 
-        res.status(201).json({
-            message: "Berhasil menambahkan data transaksi"
-        });
-    }catch (error) {
-        res.status(404).json({
-            message: error.message
-        });
-    }
-}
+    let transaksi = await model.transaksi.create(data);
 
-controller.createBulkDetTransaksi = async function(req, res){
-    try{
-        let transaksi = await model.detTransaksi.bulkCreate(req.body);
-        res.status(201).json({
-            message: "Berhasil menambahkan seluruh transaksi",
-            data: transaksi
-        });
-    }catch (error) {
-        res.status(404).json({
-            message: error.message
-        });
-    }
-}
+    res.status(201).json({
+      message: 'Berhasil menambahkan data transaksi',
+      id_transaksi: data.id_transaksi,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
+
+controller.createBulkDetTransaksi = async function (req, res) {
+  try {
+    let transaksi = await model.detTransaksi.bulkCreate(req.body);
+    res.status(201).json({
+      message: 'Berhasil menambahkan seluruh transaksi',
+      data: transaksi,
+    });
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+};
 
 export default controller;
--- routes/transaksiRouter.js
+++ routes/transaksiRouter.js
@@ -1,14 +1,14 @@
-import express from "express";
-import multer from "multer";
-import controller from "../controller/index.js";
+import express from 'express';
+import multer from 'multer';
+import controller from '../controller/index.js';
 
 const route = express.Router();
 const upload = multer();
 
-route.post("/transaksi/detail", upload.none(), controller.transaksi.getDetailTransaksi);
-route.post("/transaksi/detail", upload.none(), controller.transaksi.createBulkDetTransaksi);
+route.post('/transaksi/detail', upload.none(), controller.transaksi.getDetailTransaksi);
+route.post('/create/transaksi/detail', upload.none(), controller.transaksi.createBulkDetTransaksi);
 
-route.post("/transaksi", upload.none(), controller.transaksi.getTransaksi);
-route.post("/transaksi", upload.none(), controller.transaksi.createTransaksi);
+route.post('/transaksi', upload.none(), controller.transaksi.getTransaksi);
+route.post('/create/transaksi', upload.none(), controller.transaksi.createTransaksi);
 
 export default route;
